2d801d59fa569a99c5f45723393e7735c37cc3a5
armaBurton <armanarma@armaburton.com>
2022-01-27 11:42:22 -0800

    first list finished, list of dragons pre 1900s

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import DragonList from './DragonList';
 // import your arrays here
+import { dragonListOfStrings } from './dragons-list-of-strings';
 
 function App() {
   return (
     <div className="App">
         Render all your lists here. Pass the arrays as props.
+      <DragonList dragons={dragonListOfStrings} />
     </div>
   );
 }
---
+++ src/Dragons.js
@@ -0,0 +1,6 @@
+export default function Dragons(prop){
+  console.log(prop);
+  return <li>
+    {prop.dragon}
+  </li>;
+}
