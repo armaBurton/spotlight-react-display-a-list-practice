9c72044cbb53a0857c659636bdfb43c1d2834b91
armaBurton <armanarma@armaburton.com>
2022-01-27 22:09:48 -0800

    testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('The dragons in the underground lake whose fighting upsets Uther Pendragon\'s tower, as revealed by Merlin; Geoffrey of Monmouth, Historia Regum Britanniae (c.1136)', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/underground lake whose /i);
   expect(linkElement).toBeInTheDocument();
 });
