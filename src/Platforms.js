b9085d06e7adfbbe5397dd9a359a911c8b11c384
armaBurton <armanarma@armaburton.com>
2022-01-27 22:03:31 -0800

    testing

--- src/Platforms.js
+++ src/Platforms.js
@@ -7,12 +7,10 @@ import Other from './Other';
 export default function Platforms(props){
   return <>
     <h5>Platforms:</h5>
-  
     <Console console={props.platforms.console}/>
     <Computer computer={props.platforms.computer}/>
     <Handheld handheld={props.platforms.handheld}/>
     <Mobile mobile={props.platforms.mobile}/>
     <Other other={props.platforms.other}/>
-    
   </>;
 }
