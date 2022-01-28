5a319e75122779d057a140981b23b99e48f74c32
armaBurton <armanarma@armaburton.com>
2022-01-27 16:22:51 -0800

    pushing up the GTA array

---
+++ src/GTAList.js
@@ -0,0 +1,93 @@
+export const GTAList = [
+  {
+    title: `Grand Theft Auto`,
+    year: 1997,
+    developer: `DMA Design`,
+    platforms:{
+      consol: [`PlayStation 1`],
+      computer: [`Windows`, `MS-DOS`],
+      handheld: [`Game Boy Color`],
+      mobile: [],
+      other: []
+    },
+    universe: `2D`
+  },
+  {
+    title: `Grand Theft Auto 2`,
+    year: 1999,
+    developer: `DMA Design`,
+    platforms:{
+      consol: [`PlayStation 1`, `Dreamcast`],
+      computer: [`Windows`],
+      handheld: [`Game Boy Color`],
+      mobile: [],
+      other: []
+    },
+    universe: `2D`
+  },
+  {
+    title: `Grand Theft Auto III`,
+    year: 2001,
+    developer: `Rockstar North`,
+    platforms:{
+      consol: [`PlayStation 2`, `Xbox`],
+      computer: [`Windows`, `OS X`],
+      handheld: [],
+      mobile: [`Android`, `iOS`, `Fire OS`],
+      other: []
+    },
+    universe: `3D`
+  },
+  {
+    title: `Grand Theft Auto: Vice City`,
+    year: 2001,
+    developer: `Rockstar North`,
+    platforms:{
+      consol: [`PlayStation 2`, `Xbox`],
+      computer: [`Windows`, `OS X`],
+      handheld: [],
+      mobile: [`Android`, `iOS`, `Fire OS`],
+      other: []
+    },
+    universe: `3D`
+  },
+  {
+    title: `Grand Theft Auto: San Andreas`,
+    year: 2004,
+    developer: `Rockstar North`,
+    platforms:{
+      consol: [`PlayStation 2`, `PlayStation 3`, `Xbox`, `Xbox 360`],
+      computer: [`Windows`, `OS X`],
+      handheld: [],
+      mobile: [`Android`, `iOS`, `Fire OS`, `WP`],
+      other: []
+    },
+    universe: `3D`
+  },
+  {
+    title: `Grand Theft Auto IV`,
+    year: 2004,
+    developer: `Rockstar North`,
+    platforms:{
+      consol: [`PlayStation 3`, `Xbox 360`],
+      computer: [`Windows`],
+      handheld: [],
+      mobile: [],
+      other: []
+    },
+    universe: `HD`
+  },
+  {
+    title: `Grand Theft Auto V`,
+    year: 2004,
+    developer: `Rockstar North`,
+    platforms:{
+      consol: [`PlayStation 3`, `PlayStation 4`, `PlayStation 5`, `Xbox 360`, `Xbox One`, `Xbox Series X/S`],
+      computer: [`Windows`],
+      handheld: [],
+      mobile: [],
+      other: []
+    },
+    universe: `HD`
+  },
+];
